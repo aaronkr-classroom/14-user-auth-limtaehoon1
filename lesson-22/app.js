d8736b5dee029da0c7124b36a77ae25163f00e92
oshell0308 <163230836+limtaehoon1@users.noreply.github.com>
2024-06-12 05:02:02 +0000

    lesson-22 끝

--- lesson-22/app.js
+++ lesson-22/app.js
@@ -35,12 +35,34 @@ router.use(
  * @TODO: Listing 22.1 (p. 325)
  * app.js에서의 플래시 메시지 요청
  */
+const expressSession = require("express-session"),
+  cookieParser = require("cookie-parser"),
+  connectFlash = require("connect-flash");
 
+router.use(cookieParser("secret_passcode"))
+router.use(
+
+  expressSession({
+    secret: "scret_passcode",
+    cookie: {
+      maxAge : 4000000
+    },
+    resave :false,
+    saveUninitialized :false
+
+  })
+
+)
+router.use(connectFlash());
 /**
  * @TODO: Listing 22.2 (p. 327)
  * 응답상에서 connectFlash와 미들웨어와의 연계
  */
-
+router.use((req,res,next)=> {
+  res.locals.flashMessages =req.flash();
+  next();
+  
+})
 /**
  * =====================================================================
  * Define Mongoose and MongoDB connection
@@ -48,18 +70,14 @@ router.use(
  */
 
 // 애플리케이션에 Mongoose 설정
-const mongoose = require("mongoose"), // mongoose를 요청
-  dbName = "aaronkr";
-
-// 데이터베이스 연결 설정
-mongoose.connect(`mongodb://127.0.0.1:27017/${dbName}`, {
-  useNewUrlParser: true,
-});
-
-// 연결되면 메시지를 보냄
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://lim021298:ERBJYBO43EvfIngF@ut-node.z7jjt1w.mongodb.net/?retryWrites=true&w=majority&appName=ut-node",
+  {}
+);
 const db = mongoose.connection;
 db.once("open", () => {
-  console.log(`Connected to ${dbName} MongoDB using Mongoose!`);
+  console.log("Connect to MONGODB!!!");
 });
 
 /**
